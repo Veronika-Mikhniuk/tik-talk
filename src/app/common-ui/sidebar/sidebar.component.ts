fc73c1597f8a1ed90d6fca0f2f8d5b75ed3c185d
Vernika-Mikhniuk <veronika.mikhnuyk@gmail.com>
2025-05-14 15:24:24 +0200

    feat: added sidebar menu items layout

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { NgFor } from '@angular/common'
+import { SvgIconComponent } from "../svg-icon/svg-icon.component"
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [NgFor, SvgIconComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-
+  menuItems = [
+    {
+      label: 'Моя страница',
+      icon: 'home',
+      link: ''
+    },
+    {
+      label: 'Чаты',
+      icon: 'chats',
+      link: 'chats'
+    },
+    {
+      label: 'Поиск',
+      icon: 'search',
+      link: 'search'
+    }
+  ]
 }
